c7de1f8f7a90a91778707a741b0909c3bad8045c
Iris G <iguiller@my.centennialcollege.ca>
2022-10-19 10:20:45 -0400

    updated code for business-view

--- app/controllers/business.controller.server.js
+++ app/controllers/business.controller.server.js
@@ -9,3 +9,7 @@ export function showBusinessList (req, res, next){
         res.render('index',{title: 'Business List', page: 'business/businesslist', business: businessCollection});
     })
 }
+
+export function showBusinessUpdatePage(req, res, next ){
+    res.render('index', {title: 'Update Business Info', page:'business/updates', business: {} });
+}
--- app/routes/business.route.server.js
+++ app/routes/business.route.server.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { showBusinessList } from "../controllers/business.controller.server.js";
+import { showBusinessList, showBusinessUpdatePage } from "../controllers/business.controller.server.js";
 
 const router = Router();
 
 router.get('/business-list', showBusinessList);
+router.get('/business-updates', showBusinessUpdatePage);
 
 export default router;
